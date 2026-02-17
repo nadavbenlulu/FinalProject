a3baa25d060e5214ff5bb0231568c3fb8a8fb38e
nadav ben lulu <nadavbenlulu17@gmail.com>
2026-02-17 12:23:28 +0200

    added login-only home page and moved products to a separate page

--- api/v1/controllers/product.js
+++ api/v1/controllers/product.js
@@ -2,25 +2,25 @@ const mongoose = require('mongoose');
 const Product = require('../models/product');
 
 const obj = {
-    // שליפת כל המוצרים
+    // --- שלב 1: תיקון שליפת כל המוצרים ---
     getALLProduct: (req, res) => {
         Product.find().lean().then((products) => {
-            // שים לב: שיניתי ל-'index' כי זה הדף הראשי שבו בנינו את הרשימה
-            return res.status(200).render('index', { layout: 'main', products });
+            // שיניתי מ-'index' ל-'products' כדי שזה יציג את הקטלוג המלא אחרי הלוגין
+            return res.status(200).render('products', { 
+                layout: 'main', 
+                products,
+                pageTitle: "קטלוג מוצרים" 
+            });
         });
     },
 
-    // שליפת מוצר בודד לפי ID (הפונקציה שמעדכנת את הדף המעוצב החדש)
+    // שליפת מוצר בודד לפי ID
     getproductbyid: (req, res) => {
         const id = req.params.id;
-        
-        // חיפוש לפי ה-ID המובנה של מונגו (_id) כדי למנוע טעויות
         Product.findById(id).lean().then((prod) => {
             if (!prod) {
                 return res.status(404).send("מוצר לא נמצא");
             }
-            console.log("Found product:", prod.pname);
-            // שליחה לעיצוב החדש ב-product.handlebars
             return res.status(200).render('product', { 
                 layout: 'main', 
                 pid: id, 
@@ -38,7 +38,7 @@ const obj = {
             if (data.length > 0) {
                 return res.status(200).json({ message: `product id ${pid} already exist` });
             } else {
-                Product.create(req.body).then((prod) => { // השתמש ב-create במקום insertOne
+                Product.create(req.body).then((prod) => {
                     return res.status(200).json(prod);
                 });
             }
@@ -61,66 +61,66 @@ const obj = {
 };
 
 module.exports = obj;
+// const mongoose = require('mongoose');
+// const Product = require('../models/product');
 
+// const obj = {
+//     // שליפת כל המוצרים
+//     getALLProduct: (req, res) => {
+//         Product.find().lean().then((products) => {
+//             // שים לב: שיניתי ל-'index' כי זה הדף הראשי שבו בנינו את הרשימה
+//             return res.status(200).render('index', { layout: 'main', products });
+//         });
+//     },
 
-
-
-// const mongoose=require('mongoose');
-// const Product=require('../models/product');
-// const product = require('../models/product');
-
-// const obj={
-// getALLProduct:(req,res)=>{
-//    Product.find().lean().then((data)=>{
-//       return res.status(200).render('products',{layout:'main',data});
-//    })
+//     // שליפת מוצר בודד לפי ID (הפונקציה שמעדכנת את הדף המעוצב החדש)
+//     getproductbyid: (req, res) => {
+//         const id = req.params.id;
+        
+//         // חיפוש לפי ה-ID המובנה של מונגו (_id) כדי למנוע טעויות
+//         Product.findById(id).lean().then((prod) => {
+//             if (!prod) {
+//                 return res.status(404).send("מוצר לא נמצא");
+//             }
+//             console.log("Found product:", prod.pname);
+//             // שליחה לעיצוב החדש ב-product.handlebars
+//             return res.status(200).render('product', { 
+//                 layout: 'main', 
+//                 pid: id, 
+//                 prod: prod 
+//             });
+//         }).catch(err => {
+//             console.error(err);
+//             res.status(500).send("שגיאה בשליפת המוצר");
+//         });
 //     },
 
 //     addNewProduct: (req, res) => {
-//   const pid = req.body.pid;
-// //לפני הוספת מוצר חדש נבדוק האם קיים מוצר אם אותו קוד מוצר
-//   Product.find({ pid: pid }).then((data) => {
-//     if (data.length > 0) {
-//       return res
-//         .status(200)
-//         .json({ message: `product id ${pid} already exist` });
-//     } else {
-//       Product.insertOne(req.body).then((prod) => {
-//         return res.status(200).json(prod);
-//       });
+//         const pid = req.body.pid;
+//         Product.find({ pid: pid }).then((data) => {
+//             if (data.length > 0) {
+//                 return res.status(200).json({ message: `product id ${pid} already exist` });
+//             } else {
+//                 Product.create(req.body).then((prod) => { // השתמש ב-create במקום insertOne
+//                     return res.status(200).json(prod);
+//                 });
+//             }
+//         });
+//     },
+
+//     updateProductById: (req, res) => {
+//         const id = req.params.id;
+//         Product.findByIdAndUpdate(id, req.body, { new: true }).then((prod) => {
+//             return res.status(200).json(prod);
+//         });
+//     },
+
+//     deleteProductById: (req, res) => {
+//         const id = req.params.id;
+//         Product.findByIdAndDelete(id).then((prod) => {
+//             return res.status(200).json(prod);
+//         });
 //     }
-//   });
-// },
+// };
 
-//     addProductById:  (req,res)=>{
-//        const pid=req.params.id;
-//       Product.insertOne(req.body).then((prod)=>{
-//          return res.status(200).json(prod);
-//       });
-//     },    
-//     updateProductById:(req,res)=>{
-//       const pid=req.params.id;
-//       Product.updateOne({pid},req.body).then((prod)=>{
-//          return res.status(200).json(prod);
-//       });
-        
-//    },
-
-// //חדש
-// deleteProductById:(req,res)=>{
-//       const pid=req.params.id;
-//       Product.deleteOne({pid}).then((prod)=>{
-//          return res.status(200).json(prod);
-//       })
-//     } ,
-
-//     //חדש קוד 
-//     getproductbyid: (req,res)=>{
-//       const id=req.params.id;
-//       Product.find({pid:id}).lean().then((prod)=>{
-//         console.log(prod[0]);
-//          return res.status(200).render('product',{layout:'main',pid:id,prod:prod[0]});
-//          })
-//     } ,
-// }
-// module.exports=obj
+// module.exports = obj;
